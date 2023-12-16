2f1d7c7982be818a8b35d757d019c00409ec9cb7
Romek <129655936+romekdomasik@users.noreply.github.com>
2023-12-16 21:24:35 +0100

    Auto stash before merge of "dev" and "origin/dev"

--- HealthBuddy/HealthBuddy/Views/TopBar/TopBar View/Settings/SettingsListView.swift
+++ HealthBuddy/HealthBuddy/Views/TopBar/TopBar View/Settings/SettingsListView.swift
@@ -13,24 +13,63 @@
 import SwiftUI
 
 struct SettingsListView: View {
+    
+    @State private var isNotificationSwitchOn = false
+    @State private var isMusicSwitchOn = false
+    @State private var isNotifExternesSwitchOn = false
+    @State private var isScoreHiddenSwitchOn = false
+    @State private var isWatchAppSwitchOn = false
+    @State private var sleepHours = 7
+    
     var body: some View {
         VStack{
             List {
-                Text("Élément 1")
-                Text("Élément 2")
-                Text("Élément 3")
-                Text("Élément 4")
-                Text("Élément 5")
-                Text("Élément 6")
-                Text("Élément 7")
-                Text("Élément 8")
-                Text("Élément 9")
-                Text("Élément 10")
+                CustomRowView(text: "Notifications", isSwitchOn: $isNotificationSwitchOn)
+                CustomRowView(text: "Music", isSwitchOn: $isMusicSwitchOn)
+                CustomRowView(text: "External notification", isSwitchOn: $isNotifExternesSwitchOn)
+                CustomRowView(text: "Hidden score", isSwitchOn: $isScoreHiddenSwitchOn)
+                CustomRowView(text: "Watch app", isSwitchOn: $isWatchAppSwitchOn)
+                SleepOptionRowView(hours: $sleepHours)
+                
             }
         }
     }
 }
 
-#Preview {
-    SettingsListView()
+struct CustomRowView: View {
+    var text: String
+    @Binding var isSwitchOn: Bool
+
+    var body: some View {
+        HStack {
+            Text(text)
+            Spacer()
+            Toggle("", isOn: $isSwitchOn)
+                .labelsHidden()
+        }
+    }
+}
+
+struct SleepOptionRowView: View {
+    @Binding var hours: Int
+    
+    var body: some View {
+        HStack {
+            Text("Sleep")
+            Spacer()
+            Picker("", selection: $hours) {
+                ForEach(1..<8) { hour in
+                    Text("\(hour)h\(hour == 1 ? "" : "")")
+                }
+            }
+            .labelsHidden()
+            .pickerStyle(SegmentedPickerStyle())
+        }
+    }
+}
+
+struct SettingsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsListView()
+    }
 }
