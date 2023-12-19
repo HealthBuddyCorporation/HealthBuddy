2394c5c4c5486747fc08ecb0ebc320d6804b2859
Raphael Fassotte <raphael.fassote@icloud.com>
2023-12-19 15:44:49 +0100

    Added Podium to leaderboard
    Correction of color for score view and segment
    Adding Podium to local segment

--- HealthBuddy/HealthBuddy/Views/BottomBar/BottomBar View/ScoreView.swift
+++ HealthBuddy/HealthBuddy/Views/BottomBar/BottomBar View/ScoreView.swift
@@ -11,8 +11,8 @@ struct ScoreView: View {
     @EnvironmentObject var data :DataModel
     @State private var selectedSegment = 0
     let friendsSegment = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6","Item 7", "Item 8", "Item 9", "Item 10"]
-    let localSegment = ["Text 1", "Text 2", "Text 3"]
-    let nationalSegment = Image(systemName: "photo")
+    let localSegment = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6","Item 7", "Item 8", "Item 9", "Item 10"]
+    let nationalSegment = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6","Item 7", "Item 8", "Item 9", "Item 10"]
     
     var body: some View {
         ZStack {
@@ -147,47 +147,143 @@ struct ScoreView: View {
                     .padding()
                     
                 case 1:
-                    ForEach(localSegment, id: \.self) { text in
-                        Text(text)
+                    HStack{
+                        VStack{
+                            Text("\(data.scoreList[2].username)")
+                                .font(.title2)
+                                .foregroundColor(.white)
+                                .bold()
+                            ProfilPictureView(profilePic: data.scoreList[2].profilePicture)
+                            
+                            ZStack{
+                                BlurEffect(style: .systemMaterialLight)
+                                    .frame(width: 100, height: 80)
+                                    .cornerRadius(2)
+                                ZStack{
+                                    Text("Third")
+                                        .foregroundColor(.gray)
+                                        .bold()
+                                        .padding(EdgeInsets(top: -30, leading: 0, bottom: 0, trailing: 0))
+                                    Text("Score")
+                                        .font(.headline)
+                                        .foregroundColor(.gray)
+                                    
+                                    Text("\(data.scoreList[2].score)")
+                                        .font(.headline)
+                                        .foregroundColor(.gray)
+                                        .padding(EdgeInsets(top: 40, leading: 0, bottom: 0, trailing: 0))
+                                }
+                            }
+                        }.padding(EdgeInsets(top: 80, leading: 0, bottom: 0, trailing: 0))
                         
+                        VStack{
+                            Text("\(data.scoreList[0].username)")
+                                .font(.title2)
+                                .foregroundColor(.white)
+                                .bold()
+                            ProfilPictureView(profilePic: "\(data.scoreList[0].profilePicture)")
+                            
+                            ZStack {
+                                BlurEffect(style: .systemMaterialLight)
+                                    .frame(width: 100, height: 160)
+                                    .cornerRadius(2)
+                                Text("First")
+                                    .foregroundColor(.gray)
+                                    .bold()
+                                    .padding(EdgeInsets(top: -60, leading: 0, bottom: 0, trailing: 0))
+                                Text("Score")
+                                    .font(.headline)
+                                    .foregroundColor(.gray)
+                                    .padding(EdgeInsets(top: 80, leading: 0, bottom: 0, trailing: 0))
+                                Text("\(data.scoreList[0].score)")
+                                    .font(.headline)
+                                    .foregroundColor(.gray)
+                                    .padding(EdgeInsets(top: 120, leading: 0, bottom: 0, trailing: 0))
+                            }
+                        }
+                        
+                        VStack{
+                            Text("\(data.scoreList[1].username)")
+                                .font(.title2)
+                                .foregroundColor(.white)
+                                .bold()
+                            ProfilPictureView(profilePic: "\(data.scoreList[1].profilePicture)")
+                            
+                            ZStack {
+                                BlurEffect(style: .systemMaterialLight)
+                                    .frame(width: 100, height: 120)
+                                    .cornerRadius(2)
+                                ZStack {
+                                    Text("Second")
+                                        .foregroundColor(.gray)
+                                        .bold()
+                                        .padding(EdgeInsets(top: -40, leading: 0, bottom: 0, trailing: 0))
+                                    Text("Score")
+                                        .font(.headline)
+                                        .foregroundColor(.gray)
+                                        .padding(EdgeInsets(top: 40, leading: 0, bottom: 0, trailing: 0))
+                                    Text("\(data.scoreList[1].score)")
+                                        .font(.headline)
+                                        .foregroundColor(.gray)
+                                        .padding(EdgeInsets(top: 80, leading: 0, bottom: 0, trailing: 0))
+                                }
+                            }
+                        }.padding(EdgeInsets(top: 40, leading: 0, bottom: 0, trailing: 0))
                     }
-                    .padding()
-                        List {
-                        Text("Item 1")
-                        Text("Item 2")
-                        Text("Item 3")
-                        Text("Item 4")
-                        Text("Item 5")
-                        Text("Item 6")
-                        Text("Item 7")
-                        Text("Item 8")
-                        Text("Item 9")
-                        Text("Item 10")
-                    }.listRowBackground(Color.white.opacity(0.5))
-                    .scrollContentBackground(.hidden)
-                    .padding()
                     
-                case 2:
-                    nationalSegment
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 100)
-                        .padding()
-                    
-                    List {
-                        Text("Item 1")
-                        Text("Item 2")
-                        Text("Item 3")
-                        Text("Item 4")
-                        Text("Item 5")
-                        Text("Item 6")
-                        Text("Item 7")
-                        Text("Item 8")
-                        Text("Item 9")
-                        Text("Item 10")
+                    List(localSegment, id: \.self) { item in
+                        
+                        DisclosureGroup(item) {
+                            HStack{
+                                Text("Last run :")
+                                    .font(.headline)
+                                Text("Today")
+                            }
+                            HStack{
+                                Text("Record :")
+                                    .font(.headline)
+                                Text("2300 Buddys")
+                            }
+                            HStack{
+                                Text("Like :")
+                                    .font(.headline)
+                                Button("Like Me") {
+                                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                                }
+                            }
+                        }.listRowBackground(Color.white.opacity(0.5))
                     }.scrollContentBackground(.hidden)
                     .padding()
                     
+                case 2:
+                    Image(systemName: "photo.artframe")
+                        .resizable()
+                        .frame(width: 250, height: 200)
+                        .foregroundColor(.black .opacity(0.6))
+                    
+                    List(nationalSegment, id: \.self) { item in
+                        
+                        DisclosureGroup(item) {
+                            HStack{
+                                Text("Last run :")
+                                    .font(.headline)
+                                Text("Today")
+                            }
+                            HStack{
+                                Text("Record :")
+                                    .font(.headline)
+                                Text("2300 Buddys")
+                            }
+                            HStack{
+                                Text("Like :")
+                                    .font(.headline)
+                                Button("Like Me") {
+                                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                                }
+                            }
+                        }.listRowBackground(Color.white.opacity(0.5))
+                    }.scrollContentBackground(.hidden)
+                    .padding()
                     
                 default:
                     EmptyView()
@@ -202,3 +298,4 @@ struct ScoreView: View {
 #Preview {
     ScoreView()
 }
+
